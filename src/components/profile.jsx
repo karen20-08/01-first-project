0db46704f275b305b8ad41e3c3d7ffab1f7bdfef
Karen <karen20-08@yandex.ru>
2020-05-04 19:43:39 +0400

    created nwe project

---
+++ src/components/profile.jsx
@@ -0,0 +1,26 @@
+import React from 'react';
+
+const Profile = () => {
+    return             <div className='content'>
+    <div> 
+      <img src='https://image.shutterstock.com/image-photo/bright-spring-view-cameo-island-260nw-1048185397.jpg'/>
+         </div>
+   <div>
+     <img src='https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcQYJlgh5AVnwhfACl71fmhnQdn80Dj0gSSkPLxlYWLbkoStNtCs&usqp=CAU'/>
+     </div> 
+     <div>
+       My post 
+       
+     <div>
+     New postj
+      </div> 
+     <div>
+       <div>post 1</div>
+       <div> post 2</div>
+     </div>
+</div>
+</div>
+}
+
+
+export default Profile;
